8f1c903815ea6224da32f1f5646991f232f21eff
shogoshigemitsu <shogo.shigemitsu@gmail.com>
2018-08-22 08:01:32 +0000

    Add hello

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,7 @@
-require_relative 'boot'
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
-require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
-module ToyApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  def hello
+    render html: "hello, world!"
   end
 end
